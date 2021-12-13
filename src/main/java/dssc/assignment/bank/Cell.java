891209ab96b6b77d507bb7bf9ee847daa0f3a3f8
Dario Campagna <campagna@esteco.com>
2021-12-13 10:27:11 +0100

    We now parse all the digits from 0 to 9

--- src/main/java/dssc/assignment/bank/Cell.java
+++ src/main/java/dssc/assignment/bank/Cell.java
@@ -17,10 +17,45 @@ public class Cell {
                     " _ " +
                     " _|" +
                     "|_ ";
+    private static final String THREE_CELL =
+                    " _ " +
+                    " _|" +
+                    " _|";
+    private static final String FOUR_CELL =
+                    "   " +
+                    "|_|" +
+                    "  |";
+    private static final String FIVE_CELL =
+                    " _ " +
+                    "|_ " +
+                    " _|";
+    private static final String SIX_CELL =
+                    " _ " +
+                    "|_ " +
+                    "|_|";
+    private static final String SEVEN_CELL =
+                    " _ " +
+                    "  |" +
+                    "  |";
+    private static final String EIGHT_CELL =
+                    " _ " +
+                    "|_|" +
+                    "|_|";
+    private static final String NINE_CELL =
+                    " _ " +
+                    "|_|" +
+                    " _|";
     private static final Map<String, String> cellTextToString = new HashMap<>() {{
         put(ZERO_CELL, "0");
         put(ONE_CELL, "1");
         put(TWO_CELL, "2");
+        put(THREE_CELL, "3");
+        put(FOUR_CELL, "4");
+        put(FIVE_CELL, "5");
+        put(SIX_CELL, "6");
+        put(SEVEN_CELL, "7");
+        put(EIGHT_CELL, "8");
+        put(NINE_CELL, "9");
     }};
 
     private String cellAsText;
--- src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
+++ src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
@@ -34,4 +34,67 @@ public class ParseCellTest {
                         "|_ ";
         assertEquals("2", new Cell(twoAsText).toString());
     }
+
+    @Test
+    void threeCell() {
+        String threeAsText =
+                        " _ " +
+                        " _|" +
+                        " _|";
+        assertEquals("3", new Cell(threeAsText).toString());
+    }
+
+    @Test
+    void fourCell() {
+        String fourAsText =
+                        "   " +
+                        "|_|" +
+                        "  |";
+        assertEquals("4", new Cell(fourAsText).toString());
+    }
+
+    @Test
+    void fiveCell() {
+        String fiveAsText =
+                        " _ " +
+                        "|_ " +
+                        " _|";
+        assertEquals("5", new Cell(fiveAsText).toString());
+    }
+
+    @Test
+    void sixCell() {
+        String sixAsText =
+                        " _ " +
+                        "|_ " +
+                        "|_|";
+        assertEquals("6", new Cell(sixAsText).toString());
+    }
+
+    @Test
+    void sevenCell() {
+        String sevenAsText =
+                        " _ " +
+                        "  |" +
+                        "  |";
+        assertEquals("7", new Cell(sevenAsText).toString());
+    }
+
+    @Test
+    void eightCell() {
+        String eightAsText =
+                        " _ " +
+                        "|_|" +
+                        "|_|";
+        assertEquals("8", new Cell(eightAsText).toString());
+    }
+
+    @Test
+    void nineCell() {
+        String nineAsText =
+                        " _ " +
+                        "|_|" +
+                        " _|";
+        assertEquals("9", new Cell(nineAsText).toString());
+    }
 }
