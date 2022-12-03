2f712c2cc66a989ff4dcfdd37b4c8adbbb324e75
Dario Campagna <campagna@esteco.com>
2022-12-03 15:45:56 +0100

    Rename test class

--- src/test/java/dssc/assignment/bank/parse/AccountNumberFromEntryTest.java
+++ src/test/java/dssc/assignment/bank/parse/AccountNumberFromEntryTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PrintAccountNumberTest {
+public class AccountNumberFromEntryTest {
 
     @Test
     void allZerosEntry() {
